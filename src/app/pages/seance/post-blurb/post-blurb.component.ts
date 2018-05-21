cac64704114d55b6f70996ea94779d12ba470d7b
Lasha <lasha854@yahoo.com>
2018-05-21 19:22:05 +0400

    Post blurb fb

--- src/app/pages/seance/post-blurb/post-blurb.component.ts
+++ src/app/pages/seance/post-blurb/post-blurb.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { SeanceService } from '../seance.service';
 
 @Component({
   selector: 'app-post-blurb',
   templateUrl: './post-blurb.component.html',
   styleUrls: ['./post-blurb.component.scss']
 })
-export class PostBlurbComponent implements OnInit {
-
-  constructor() { }
+export class PostBlurbComponent implements OnInit, AfterViewInit {
+  blurbForm: FormGroup;
+  constructor(private seanceService: SeanceService, private fb: FormBuilder) {}
 
   ngOnInit() {
+    this.blurbForm = this.fb.group({
+      text: ['', Validators.required],
+      color: ['green', Validators.required]
+    });
   }
 
+  ngAfterViewInit() {
+    this.seanceService.scrollBottom();
+  }
+
+  postBlurb() {
+    console.log(this.blurbForm.value);
+  }
 }
