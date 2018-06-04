f76c4873a0f530954571201f6e5110fa1627c31c
Lasha <lasha854@yahoo.com>
2018-06-04 18:28:51 +0400

    Separate ApolloClientModule from SharedModule

---
+++ src/app/shared/apollo-client.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { HttpLink, HttpLinkModule } from 'apollo-angular-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloLink } from 'apollo-link';
+import { onError } from 'apollo-link-error';
+import { ApolloModule, Apollo } from 'apollo-angular';
+import { environment } from '../../environments/environment';
+
+@NgModule({
+  exports: [ApolloModule, HttpLinkModule]
+})
+
+export class ApolloClientModule {
+  constructor(apollo: Apollo, httpLink: HttpLink) {
+    const baseLink = httpLink.create({ uri: environment.graphqlUrl });
+    const errorLink = onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors) {
+        graphQLErrors.map(({ message, locations, path }) =>
+          console.warn(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+          )
+        );
+      }
+      if (networkError) {
+        console.log(networkError);
+        networkError.error.errors.map(({ message, locations, path }) =>
+          console.warn(
+            `[GraphQL error]: Message: ${message}, Location: ${JSON.stringify(
+              locations
+            )}`
+          )
+        );
+      }
+    });
+    const AllLinks = ApolloLink.from([errorLink, baseLink]);
+    apollo.create({
+      link: baseLink,
+      cache: new InMemoryCache(),
+      defaultOptions: {
+        watchQuery: {
+          errorPolicy: 'all'
+        }
+      }
+    });
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,4 @@
-import { environment } from './../../environments/environment';
-import { NgModule, Injectable } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EscapeHtmlPipe } from './pipes/escape-html.pipe';
 import { TransPipe } from './pipes/trans.pipe';
@@ -7,29 +6,27 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { PasswordStrengthBarModule } from 'ng2-password-strength-bar';
 import { YoutubePlayerModule } from 'ng2-youtube-player';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { ApolloModule, Apollo } from 'apollo-angular';
-import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { ApolloLink } from 'apollo-link';
-import { onError } from 'apollo-link-error';
 import { AuthService } from './services/auth.service';
 import { JwtModule } from '@auth0/angular-jwt';
 import { LoggedInGuardService } from './guards/logged-in-guard.service';
 import { LoggedOutGuardService } from './guards/logged-out-guard.service';
-import { getOperationAST } from 'graphql';
 import { TokenInterceptor } from './interceptors';
+import { ApolloClientModule } from './apollo-client.module';
 
 @NgModule({
-  imports: [CommonModule, HttpLinkModule,
+  imports: [
+    CommonModule,
     JwtModule.forRoot({
-    config: {
-      tokenGetter: () => {
-        return localStorage.getItem('token');
-      },
-      whitelistedDomains: ['localhost:3001'],
-      blacklistedRoutes: ['localhost:3001/auth/']
-    }
-  })],
+      config: {
+        tokenGetter: () => {
+          return localStorage.getItem('token');
+        },
+        whitelistedDomains: ['localhost:3001'],
+        blacklistedRoutes: ['localhost:3001/auth/']
+      }
+    }),
+    ApolloClientModule
+  ],
   declarations: [EscapeHtmlPipe, TransPipe],
   exports: [
     CommonModule,
@@ -39,51 +36,21 @@ import { TokenInterceptor } from './interceptors';
     TransPipe,
     PasswordStrengthBarModule,
     YoutubePlayerModule,
-    HttpClientModule,
-    ApolloModule,
-    HttpLinkModule
+    HttpClientModule
   ],
-  providers: [AuthService, LoggedInGuardService, LoggedOutGuardService, {
-    provide: HTTP_INTERCEPTORS,
-    useClass: TokenInterceptor,
-    multi: true,
-  }]
+  providers: [
+    AuthService,
+    LoggedInGuardService,
+    LoggedOutGuardService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true
+    }
+  ]
 })
-
 export class SharedModule {
-  constructor(apollo: Apollo, httpLink: HttpLink) {
-    const baseLink = httpLink.create({ uri: environment.graphqlUrl });
-    const errorLink = onError(({ graphQLErrors, networkError }) => {
-      if (graphQLErrors) {
-        graphQLErrors.map(({ message, locations, path }) =>
-          console.warn(
-            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
-          )
-        );
-      }
-      if (networkError) {
-        console.log(networkError);
-        networkError.error.errors.map(({ message, locations, path }) =>
-          console.warn(
-            `[GraphQL error]: Message: ${message}, Location: ${JSON.stringify(
-              locations
-            )}`
-          )
-        );
-      }
-    });
-    const AllLinks = ApolloLink.from([errorLink, baseLink]);
-
-    apollo.create({
-      link: baseLink,
-      cache: new InMemoryCache(),
-      defaultOptions: {
-        watchQuery: {
-          errorPolicy: 'all'
-        }
-      }
-    });
-  }
+  constructor() {}
 }
 
 
