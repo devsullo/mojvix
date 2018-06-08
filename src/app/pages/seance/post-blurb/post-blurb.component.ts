02d53332a0a0b8f75c905cc9ba4138b4f0d83402
Lasha <lasha854@yahoo.com>
2018-06-08 14:35:52 +0400

    Post blurb on specific movie

--- src/app/pages/seance/post-blurb/post-blurb.component.ts
+++ src/app/pages/seance/post-blurb/post-blurb.component.ts
@@ -1,22 +1,33 @@
+import { RouteService } from './../../../shared/services/route.service';
+import { ISeance } from './../../../store/model/seance';
+import { IAppState } from './../../../store/IAppState';
 import { Location } from '@angular/common';
 import { PostBlurbService } from './post-blurb.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, OnDestroy } from '@angular/core';
 import { SeanceService } from '../seance.service';
+import { NgRedux } from 'ng2-redux';
+import { Unsubscribe } from 'redux';
 
 @Component({
   selector: 'app-post-blurb',
   templateUrl: './post-blurb.component.html',
   styleUrls: ['./post-blurb.component.scss']
 })
-export class PostBlurbComponent implements OnInit, AfterViewInit {
+export class PostBlurbComponent implements OnInit, AfterViewInit, OnDestroy {
   blurbForm: FormGroup;
   constructor(
     private seanceService: SeanceService,
     private fb: FormBuilder,
     private postBlurbService: PostBlurbService,
-    private location: Location
+    private location: Location,
+    private ngRedux: NgRedux<IAppState>,
+    private routeService: RouteService
   ) {}
+
+  get seance(): ISeance {
+    return this.ngRedux.getState().seance;
+  }
 
   ngOnInit() {
     this.blurbForm = this.fb.group({
@@ -25,15 +36,19 @@ export class PostBlurbComponent implements OnInit, AfterViewInit {
     });
   }
 
+  ngOnDestroy() { }
+
   ngAfterViewInit() {
     this.seanceService.scrollBottom();
   }
 
   postBlurb() {
-    console.log(this.blurbForm.value);
-    this.postBlurbService.createBlurb(this.blurbForm.value, 6)
-    .map(res => res.data.createBlurb)
-    .subscribe(id => {});
+    this.postBlurbService
+      .createBlurb(this.blurbForm.value, this.seance.id)
+      .map(res => res.data.createBlurb)
+      .subscribe(id => {
+        this.routeService.navigateSeance('');
+      });
   }
 
   cancel() {
